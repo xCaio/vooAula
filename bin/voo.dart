fa20cde47aa837a26dddf5ffaf6100db89601422
xcaio <mrlose22@gmail.com>
2022-04-17 19:08:37 -0300

    subindo testes para o git

--- bin/voo.dart
+++ bin/voo.dart
@@ -1,3 +1,33 @@
-void main(List<String> arguments) {
-  print('Hello world!');
+import '../lib/passageiro.dart';
+import '../lib/passagem.dart';
+
+void main(List<String> args) {
+  Passageiro passageiro = Passageiro("_cpf", "_nome", "_end", "_tel");
+  Passagem passagem = Passagem(0, 0, "VAZIO");
+
+  passagem.voo = "BH";
+  print(passagem.voo);
+
+  passagem.numero = 10;
+  print(passagem.numero);
+
+  passagem.poltrona = 12;
+  print(passagem.poltrona);
+
+  passagem.atribuirPassageiro(passageiro);
+
+  passageiro.txtInfo();
+  passageiro.cpf = "123.456.789-10";
+  print(passageiro.cpf);
+
+  passageiro.end = "Rua finalmente, 206";
+  print(passageiro.end);
+
+  passageiro.nome = "Caio";
+  print(passageiro.nome);
+
+  passageiro.tel = "31 9 9999-9999";
+  print(passageiro.tel);
+  
+
 }
